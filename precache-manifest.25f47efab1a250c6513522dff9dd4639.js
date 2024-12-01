2da4d30cf79f7d5ff5c220d3b7265bf2c0600ad0
sidd-j <53267391+sidd-j@users.noreply.github.com>
2024-12-01 11:53:19 +0530

    Updates

---
+++ precache-manifest.25f47efab1a250c6513522dff9dd4639.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d63776e8dbb8f53a12e4f4f90add25",
+    "url": "/Ubuntufest/index.html"
+  },
+  {
+    "revision": "d2827f4cecccbf186038",
+    "url": "/Ubuntufest/static/css/main.dd33decd.chunk.css"
+  },
+  {
+    "revision": "4e9fb108817f1d9e47aa",
+    "url": "/Ubuntufest/static/js/2.52e1c61b.chunk.js"
+  },
+  {
+    "revision": "da96256dd070ffac0081a3cd003f3cfd",
+    "url": "/Ubuntufest/static/js/2.52e1c61b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4674584a7891c469829b",
+    "url": "/Ubuntufest/static/js/3.c693d707.chunk.js"
+  },
+  {
+    "revision": "d2827f4cecccbf186038",
+    "url": "/Ubuntufest/static/js/main.f6b8e140.chunk.js"
+  },
+  {
+    "revision": "cc7470fdf19c1d43b74e",
+    "url": "/Ubuntufest/static/js/runtime-main.9184ae78.js"
+  }
+]);
